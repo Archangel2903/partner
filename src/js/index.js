0cb6b520e0ac75de8bc86341da8b75d907a448ca
Archangel2903 <archangel2903@gmail.com>
2023-12-28 16:55:09 +0200

    header progress

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ import 'jquery-ui'
 import 'jquery-ui/ui/effect'
 import 'bootstrap';
 import 'popper.js';
+import Parallax from "parallax-js";
 import Swiper from 'swiper/dist/js/swiper.min';
-import noUiSlider from 'nouislider';
 
 $(window).on('load', function () {
     let b = $('body');
@@ -51,53 +51,10 @@ $(function () {
         }
     }
 
-    // Range slide
-    if ($('input[type="range"]')) {
-        let sliderRange = document.querySelectorAll('.slider-range');
-        let sliderHandles = document.querySelectorAll('.slider-handles');
-
-        if (sliderRange.length) {
-            sliderRange.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let startValue = input.hasAttribute('value') ? Number(input.getAttribute('value')) : 1;
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [startValue],
-                    step: 1,
-                    behavior: 'tap',
-                    connect: [true, false],
-                    range: {
-                        'min': [minValue],
-                        'max': [maxValue]
-                    }
-                });
-            });
-        }
-
-        if (sliderHandles.length) {
-            sliderHandles.forEach(function (elem) {
-                let input = elem.childNodes[0];
-                let minValue = input.hasAttribute('min') ? Number(input.getAttribute('min')) : 1;
-                let maxValue = input.hasAttribute('max') ? Number(input.getAttribute('max')) : 100;
-
-                input.remove();
-
-                noUiSlider.create(elem, {
-                    start: [minValue, maxValue/2],
-                    step: 1,
-                    behavior: 'tap-drag',
-                    connect: true,
-                    range: {
-                        'min': minValue,
-                        'max': maxValue
-                    }
-                });
-            });
-        }
+    // ParallaxJS
+    if ($('#scene').length) {
+        const scene = document.getElementById('scene');
+        const parallaxInstance = new Parallax(scene);
     }
 
     // Lazy load observer
